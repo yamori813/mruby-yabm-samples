f50ed0e0f4fe39ff012a2285c808749ef4ffb4f1
Hiroki Mori <yamori813@yahoo.co.jp>
2024-09-11 07:03:31 +0900

    use macro

--- bcmbm/wca_kanji.rb
+++ bcmbm/wca_kanji.rb
@@ -10,16 +10,25 @@ begin
   yabm = YABM.new
 
   yabm.setbaud(9600, 1)
+  yabm.print KANJIMODE, 1
 
   loop do
-    str = 'Hello mruby'
+    str = 'あいうえお'
     yabm.print str, 1
     yabm.msleep 5_000
 
     yabm.print LF, 1
-    str = 'on bcmbm'
+    str = 'abcdef12345'
     yabm.print str, 1
     yabm.msleep 5_000
+
+    yabm.print LF, 1
+    str = '洋酒といえば、誰でも最初に思い'
+    yabm.print str, 1
+    yabm.print LF, 1
+    str = '浮かべるのがウイスキー。'
+    yabm.print str, 1
+    yabm.msleep 10_000
 
     yabm.print CLEAR, 1
     yabm.msleep 3_000
--- bcmbm/wca_keypad.rb
+++ bcmbm/wca_keypad.rb
@@ -1,10 +1,25 @@
+# control code
+ASCIIMODE = "\x00"
+KANJIMODE = "\x01"
+CLEAR = "\x0c"
+LF = "\x0d"
+BACK = "\x08"
+FORWARD = "\x09"
+
+# key code
+UPKEY = 85
+DOWNKEY = 68
+LEFTKEY =76
+RIGHTKEY = 82
+CENTERKEY = 79
+
 begin
   yabm = YABM.new
 
   yabm.setbaud(9600, 1)
 
   yabm.print 'mruby on', 1
-  yabm.print "\x0d", 1
+  yabm.print LF, 1
   yabm.print 'YABM', 1
 
   loop do
@@ -12,11 +27,11 @@ begin
 
     chr = yabm.getch(1)
     yabm.print "\x0c", 1
-    yabm.print 'UP', 1 if chr == 85
-    yabm.print 'DOWN', 1 if chr == 68
-    yabm.print 'LEFT', 1 if chr == 76
-    yabm.print 'RIGHT', 1 if chr == 82
-    yabm.print 'CENTER', 1 if chr == 79
+    yabm.print 'UP', 1 if chr == UPKEY
+    yabm.print 'DOWN', 1 if chr == DOWNKEY
+    yabm.print 'LEFT', 1 if chr == LEFTKEY
+    yabm.print 'RIGHT', 1 if chr == RIGHTKEY
+    yabm.print 'CENTER', 1 if chr == CENTERKEY
   end
 rescue StandardError => e
   yabm.print e.to_s
