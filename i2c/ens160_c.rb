8df7454128c2ef834a918f7adc0b2f3d1519809d
Hiroki Mori <yamori813@yahoo.co.jp>
2025-11-20 08:06:17 +0900

    add tem and rh

--- i2c/ens160_c.rb
+++ i2c/ens160_c.rb
@@ -15,10 +15,14 @@ class ENS160
   DATA_AQI = 0x21
   DATA_TVOC = 0x22
   DATA_ECO2 = 0x24
+  DATA_T = 0x30
+  DATA_RH = 0x32
 
   def initialize(yabm)
     @y = yabm
     @y.i2cwrite(ENS160ADDR, [OPMODE, OPMODE_IDLE])
+    # wait for Normal operation at Status
+    @y.msleep 100 while @y.i2cread(ENS160ADDR, 1, [DATA_STATUS]) & 0x0c != 0
   end
 
   def id
@@ -26,21 +30,26 @@ class ENS160
     (arr[1] << 8) | arr[0]
   end
 
-  def setTempRh(t, r)
-    val = ((((t * 1000) + 273150) * 64) + 500) / 1000
+  def setTempRh(t, rh)
+    val = ((((t * 1000) + 273_150) * 64) + 500) / 1000
     @y.i2cwrite(ENS160ADDR, [TEMP_IN, val & 0xff, val >> 8])
-    @y.i2cwrite(ENS160ADDR,  [RH_IN,  val & 0xff, val >> 8])
+    val = rh * 512
+    @y.i2cwrite(ENS160ADDR, [RH_IN, val & 0xff, val >> 8])
   end
 
   def getData
-    @y.i2cwrite(ENS160ADDR,  [OPMODE, OPMODE_STANDERD])
+    arr = @y.i2cread(ENS160ADDR, 2, [DATA_T])
+    t = (arr[1] << 8) | arr[0]
+    arr = @y.i2cread(ENS160ADDR, 2, [DATA_RH])
+    rh = (arr[1] << 8) | arr[0]
+    @y.i2cwrite(ENS160ADDR, [OPMODE, OPMODE_STANDERD])
     @y.msleep 10_000 while @y.i2cread(ENS160ADDR, 1, [DATA_STATUS]) & 0x02 == 0
     aqi = @y.i2cread(ENS160ADDR, 1, [DATA_AQI])
     arr = @y.i2cread(ENS160ADDR, 2, [DATA_TVOC])
     tvoc = (arr[1] << 8) | arr[0]
     arr = @y.i2cread(ENS160ADDR, 2, [DATA_ECO2])
     eco2 = (arr[1] << 8) | arr[0]
-    @y.i2cwrite(ENS160ADDR, [OPMODE,OPMODE_IDLE])
-    [aqi, tvoc, eco2]
+    @y.i2cwrite(ENS160ADDR, [OPMODE, OPMODE_IDLE])
+    [aqi, tvoc, eco2, t, rh]
   end
 end
