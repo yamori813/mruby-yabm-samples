0a87454deb96abd16a30980debe29e760f9737ea
Hiroki Mori <yamori813@yahoo.co.jp>
2026-04-26 15:31:51 +0900

    modify

--- rtlbm/chk_gout.rb
+++ rtlbm/chk_gout.rb
@@ -3,40 +3,36 @@
 # GPIO output check script for RTL8196E
 
 begin
+  yabm = YABM.new
 
-yabm = YABM.new
+  yabm.print "GPIO out check\r\n"
 
-yabm.print "GPIO out check\n"
+  # JTAG and all LED is GPIO
+  yabm.gpiosetsel(0x06, 0x1ffffff, 0x36db, 0x3fff)
 
-# JTAG and all LED is GPIO
-yabm.gpiosetsel(0x06, 0x1ffffff, 0x36db, 0x3fff)
+  # all pin is gpio
+  reg = 0
+  yabm.gpiosetctl(reg)
 
-# all pin is gpio
-reg = 0
-yabm.gpiosetctl(reg)
+  # all pin is out
+  reg = 0xffff
+  yabm.gpiosetdir(reg)
 
-#all pin is out
-reg = 0xffff
-yabm.gpiosetdir(reg)
-
-
-i = 0
-loop do
-  yabm.print i.to_s + "\n"
-  if i < 16 
-    reg = 1 << i
-  else
-    reg = ~(1 << (i - 16))
+  # only GPIOA and GPIOB
+  i = 0
+  loop do
+    yabm.print "#{i}\r\n"
+    2.times do
+      reg = 1 << i
+      yabm.gpiosetdat(reg)
+      yabm.msleep(2_000)
+      reg = 0
+      yabm.gpiosetdat(reg)
+      yabm.msleep(2_000)
+    end
+    i += 1
+    i = 0 if i == 16
   end
-  yabm.gpiosetdat(reg)
-  yabm.msleep(2_000)
-  i = i + 1
-  if i == 32
-    i = 0
-  end
-end
-
-rescue => e
+rescue StandardError => e
   yabm.print e.to_s
 end
-
